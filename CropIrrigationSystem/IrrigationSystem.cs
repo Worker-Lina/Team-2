a2d1a94106355124440655a84458e17f59a91b86
алина человечкова <alinasmart19@mail.ru>
2020-12-11 20:56:32 +0600

    создала новый класс IrrigationSystem

---
+++ CropIrrigationSystem/IrrigationSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CropIrrigationSystem
+{
+    public class IrrigationSystem
+    {
+        private string name;
+        private Hose hose;
+        private Trumpet trumpet;
+        private Capacity capacity;
+        private Timer timer;
+    }
+}
