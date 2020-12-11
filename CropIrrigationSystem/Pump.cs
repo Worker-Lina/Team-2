6c5e144537f1950e600443a17e1b96fff90b8bee
Nikita Boltushkin <russkiykz@gmail.com>
2020-12-11 21:02:20 +0600

    add class Pump

---
+++ CropIrrigationSystem/Pump.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CropIrrigationSystem
+{
+    class Pump
+    {
+        private double pressure;
+        private string mesh;
+        private double filtration;
+    }
+}
